e2174446e49f3b085c6a06f2e0e2221cb3110693
Athira MU <krishna6athi@gmail.com>
2024-09-25 09:40:03 +0530

    updated

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const upload = require('../middlewares/uploadMiddleware')
+const express = require("express");
+const router = express.Router();
+const upload = require("../middlewares/uploadMiddleware");
 
-const { addFood, getAllFood, deleteFood, updateFood, getFoodById } = require('../controllers/foodController');
+const {
+  addFood,
+  getAllFood,
+  deleteFood,
+  updateFood,
+  getFoodById,
+  search,
+} = require("../controllers/foodController");
 
+router.get("/search", search);
 
-router.get("/", getAllFood)
-router.get("/:id", getFoodById)
-router.post("/", upload.fields([{ name: 'mainImage' }, { name: 'image' }]), addFood);
-router.put("/:id", upload.fields([{ name: 'mainImage' }, { name: 'image' }]), updateFood);
-router.delete("/:id", deleteFood)
+router.get("/", getAllFood);
+router.get("/:id", getFoodById);
+router.post(
+  "/",
+  upload.fields([{ name: "mainImage" }, { name: "image" }]),
+  addFood
+);
+router.put(
+  "/:id",
+  upload.fields([{ name: "mainImage" }, { name: "image" }]),
+  updateFood
+);
+router.delete("/:id", deleteFood);
 
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
